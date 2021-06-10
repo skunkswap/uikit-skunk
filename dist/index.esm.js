818b675fa60ecbf20dc8def0ce3ad0bfc803951f
skunkswap <85455810+skunkswap@users.noreply.github.com>
2021-06-10 23:59:43 +0200

    Update index.esm.js

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2204,7 +2204,7 @@ var socials = [
     {
         label: "Twitter",
         icon: "TwitterIcon",
-        href: "https://twitter.com/SkunkSwapBsc",
+        href: "https://twitter.com/SkunkSwapFin",
     },
 ];
 var MENU_HEIGHT = 64;
